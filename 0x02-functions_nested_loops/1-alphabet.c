a19993706de0d1d3505a1cd10e77038f1b9b3333
Bereket Abebayehu <12kiuat@gmail.com>
2022-04-13 07:21:35 -0700

    loop 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * printing lowercase alphabet
+ */
+void print_alphabet(void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+
+_putchar('\n');
+}
