c79e49d0bab18287b019b3a72be00550c52e78ad
Bereket Abebayehu <12kiuat@gmail.com>
2022-04-18 16:20:43 -0700

    0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,10 @@
 /**
  * reset_to_98 - change the value of integer using pointer
  * @n: pointer
+ *
  * Return:void
  */
-void reset_to_98(int *n){
-*n = 98;}	 
+void reset_to_98(int *n)
+{
+*n = 98;
+}
